1658984f6bc54de67cb5028e49a873e9d1677aee
Joac2363 <joachimstorbank@gmail.com>
2024-11-09 15:53:09 +0100

    Added Models (+DTO) for product & warehouse Linked DB to project Added Datacontext

---
+++ Warehouse API/DTO/WarehouseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse_API.DTO
+{
+    public class WarehouseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
